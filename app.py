897c08508a8deff542c41ede5163c5f3a274cf6b
Willansnow <chenp276@mail2.sysu.edu.cn>
2023-12-21 11:43:29 +0800

    success_sample

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, request
-import test  # 导入你的test.py模块
+from flask import Flask, render_template, jsonify
+from test import test
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')  # 返回index.html页面
+def home():
+    return render_template("index.html")
 
-@app.route('/run_script', methods=['POST'])
-def run_script():
-    result = test.test()  # 运行test.py中的test函数
-    return str(result)  # 返回结果
+@app.route('/get_message', methods=['GET'])
+def get_message():
+    message = test()
+    return jsonify({'message': message})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 def test():
-    return "nihao"
+    return "nima"
